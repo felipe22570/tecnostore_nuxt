576648d8f576a72e4b4a09e5ddca1fd46ee7b2fb
Felipe <piedra22570@gmail.com>
2022-10-09 16:41:19 -0500

    Changing things

--- helpers/http.js
+++ helpers/http.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   baseURL: process.env.API_URL,
   headers: {
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "*",
   },
 });
 
